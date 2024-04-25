809ab994cd39f53769dde3e7e2e1f89d0d398a13
YakultGo <god1239426256@163.com>
2024-04-25 17:10:33 +0800

    读取配置和初始化日志

---
+++ config/init.go
@@ -0,0 +1,64 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/natefinch/lumberjack"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type Config struct {
+	DB struct {
+		Mysql string
+		Redis string
+	}
+}
+
+var Conf = new(Config)
+
+func InitConfig() error {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./config")
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+	if err := viper.Unmarshal(Conf); err != nil {
+		panic(err)
+	}
+	return nil
+}
+
+func InitLogger(name string) {
+	writeSyncer := getLogWriter(name)
+	encoder := getEncoder()
+	core := zapcore.NewCore(
+		encoder,
+		zapcore.NewMultiWriteSyncer(writeSyncer, zapcore.Lock(os.Stdout)), zapcore.DebugLevel,
+	)
+
+	logger := zap.New(core, zap.AddCaller())
+	zap.ReplaceGlobals(logger)
+}
+
+func getEncoder() zapcore.Encoder {
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	encoderConfig.EncodeCaller = zapcore.ShortCallerEncoder
+	return zapcore.NewConsoleEncoder(encoderConfig)
+}
+
+func getLogWriter(name string) zapcore.WriteSyncer {
+	lumberJackLogger := &lumberjack.Logger{
+		Filename:   fmt.Sprintf("./log/%s.log", name),
+		MaxSize:    1,
+		MaxBackups: 5,
+		MaxAge:     30,
+		Compress:   false,
+	}
+	return zapcore.AddSync(lumberJackLogger)
+}
